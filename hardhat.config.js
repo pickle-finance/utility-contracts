e5f23d36db25f2ba7c2bd47fce60c45f50a2de03
Aditya Rout <adi.b20612@gmail.com>
2022-02-04 12:15:47 +0530

    chore: init hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,111 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      },
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+      hardfork: "istanbul",
+      gasPrice: "auto",
+      gas: 2500000,
+    },
+    local: {
+      url: `http://localhost:8545`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [`0x${process.env.MNEMONIC}`],
+    },
+    matic: {
+      url: "https://polygon-rpc.com/",
+      accounts: [`0x${process.env.MNEMONIC}`],
+    },
+    arbitrum: {
+      url: `https://arb1.arbitrum.io/rpc/`,
+      accounts: [`0x${process.env.MNEMONIC}`],
+    },
+    metis: {
+      url: `https://andromeda.metis.io/?owner=1088`,
+      accounts: [`0x${process.env.MNEMONIC}`],
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_APIKEY,
+  },
+  paths: {
+    sources: "./src",
+    tests: "./src/tests/strategies",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    coinmarketcap: process.env.COINMARKETCAP,
+    currency: "USD",
+    gasPrice: 32,
+  },
+  preprocess: {
+    eachLine: removeConsoleLog((hre) => hre.network.name !== "hardhat" && hre.network.name !== "localhost"),
+  },
+  mocha: {
+    timeout: 20000000,
+  },
+  vyper: {
+    version: "0.2.7",
+  },
+};
